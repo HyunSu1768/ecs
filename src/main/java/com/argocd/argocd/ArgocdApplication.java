ef723c14cfe74d70f5e9d10cd79db69b02811980
HyunSu1768 <azxcv1768@gmail.com>
2023-08-24 18:05:25 +0900

    first commit

---
+++ src/main/java/com/argocd/argocd/ArgocdApplication.java
@@ -0,0 +1,13 @@
+package com.argocd.argocd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgocdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgocdApplication.class, args);
+    }
+
+}
